9244842f5726fe1a5aa4a9ac23808c9e4ea2bd92
Ganesh Vishwakarma <55539447+devAlienX@users.noreply.github.com>
2020-05-23 23:04:53 +0530

    files-added

---
+++ js/scroll-paralax.js
@@ -0,0 +1,11 @@
+// scroll
+$(document).ready(function () {
+    $('.slider').slider({ full_width: true });
+  });
+  
+  
+  // Or with jQuery
+  
+  $(document).ready(function () {
+    $('.parallax').parallax();
+  });
